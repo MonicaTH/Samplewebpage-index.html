ac8ee5a8d1a5bfedd76cf0acfe2829d698b8a1e0
MonicaTH <35308880+MonicaTH@users.noreply.github.com>
2018-01-16 14:09:13 -0600

    Update and rename index.md to index.php

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCKTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Webpage</title>
+        <link rel="stylesheet" type="text/css "href="mystyle.php"/>
+        <link rel="icon" href="assets/images/fav.png" type="image.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/css/jquery.fullPage.js">
+        <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <link rel="stylesheet" href="assets/owl.carousel.css">
+        <script>$(document).ready(function(){$(".pagenav1").click(function() {
+                $('html, body').animate({
+                scrollTop: $('.page1').offset().top-100
+                }, 1000);
+                });
+                });
+                $(document).ready(function(){$(".pagenav2").click(function() {
+                $('html, body').animate({
+                scrollTop: $('.page2').offset().top-100
+                }, 1000);
+                });
+                });$(document).ready(function(){$(".pagenav3").click(function() {
+                $('html, body').animate({
+                scrollTop: $('.page3').offset().top-100
+                }, 1000);
+                });
+                });$(document).ready(function(){$(".pagenav4").click(function() {
+                $('html, body').animate({
+                scrollTop: $('.page4').offset().top-100
+                }, 1000);
+                });
+                });
+        </script>
+    </head>
+    <body>
+        <header class="logosec">
+            <div class="container">
+               <div class="row">   
+               <div class="col-sm-4">
+                   <div class="logo">
+                      <img src="assets/images/ini.png" alt="logo">
+                    </div>
+               </div>
+        
+        <div class="col-sm-8">
+        <nav class="nav fordesktop">
+        <!—- navigation bar —-!>
+        
+        <ul id="menu">
+        
+                <li> <a class="active pagenav1">Navlink1</a></li>
+                <li> <a class="active pagenav2">Navlink2</a></li>
+                <li> <a class="active pagenav3">Navlink3</a></li>
+                <li> <a class="active pagenav4">Navlink4</a></li> 
+              </ul>
+        </nav>
+        <nav class="nav formobile">
+        <!—- navigation bar —-!>
+        
+        <ul id="menu">
+                <li><p>Menu</p>
+                <ul>
+                <li> <a class="active pagenav1">Navlink1</a></li>
+                <li> <a class="active pagenav2">Navlink2</a></li>
+                <li> <a class="active pagenav3">Navlink3</a></li>
+                <li> <a class="active pagenav4">Navlink4</a></li> 
+               
+                </ul>
+                </li>
+              </ul>
+        </nav>
+        </div>
+        </div>
+        </div>
+        </header>
+       <p class="page1"> 
+             <h1  class="bannertext">ALL OF LIFE IS A CONSTANT EDUCATION <br>
+             -Eleanor Roosevelt</h1>
+             </p>
+       <div class="page2">
+       <p class="bannertext2"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       <p class="sec2text">
+            <img src ="assets/images/11.png" alt="sport" class="left">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting, 
+             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+              Aldus PageMaker including versions of Lorem Ipsum.
+              <img src ="assets/images/22.png" alt="sport" class="right">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+               It has survived not only five centuries, but also the leap into electronic typesetting, 
+               remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+               sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                </p>
+                </p>
+                </div>
+              <section class="page3" class="section">
+                          <div class="container">
+                                <section class="bodypart">
+                                        
+                                          <div class="mainheading">
+                                          <h2 class="imggal"><center> Sample Image Gallery </center> </h2>
+                                        </div>
+                                        
+                                        <div class="eachInlineImg"> 
+                                        <img src="assets/images/1.png" alt="empirestate" >
+                                
+                                        </div>
+                                        <div class="eachInlineImg">
+                                        <img src="assets/images/1.png" alt="statue of liberty" >
+                                        
+                                        </div>
+                                        <div class="eachInlineImg">
+                                        <img src="assets/images/1.png" alt="world trade center" >
+                                       
+                                        </div>
+                            
+                                        <div class="eachInlineImg"> 
+                                        <img src="assets/images/bb.jpg" alt="empirestate" >
+                                
+                                        </div>
+                                        <div class="eachInlineImg">
+                                        <img src="assets/images/bz.jpg" alt="statue of liberty" >
+                                        
+                                        
+                                        </div>
+                                        <div class="eachInlineImg">
+                                        <img src="assets/images/ts.jpg" alt="world trade center" >
+                                        
+                                       
+                                        </div>                                                                                        
+                                     </div>
+                                     </section>
+                <section class="page4" class="section">
+               <p class="text4"> <img src ="assets/images/11.png" alt="sport" style="float:left;width:150px;height:150px;">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting, 
+             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+              Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+              <footer class="container-fluid text-center">
+                                <div class="rows">
+                                        <div class="col-sm-4">
+                            
+                                        <ul>
+                                          <li><a href="privacypolicy.php?f=privacypolicy.php" target="_blank">Privacy Policy</a>
+                                          </li><br>
+                                          <li><a href="termsofuse.php?f=termsofuse.php" target="_blank">Terms of Use</a>
+                                          </li><br>
+                                          <li><a href="legalnotice.php?f=legalnotice.php" target="_blank">Legal Notice</a>
+                                          </li>
+                                          <li><a href="contactus.php?f=contactus.php" target="_blank">ContactUs</a>
+                                          </li>
+                                        </ul>
+                                      </div>
+                          </footer>
+                          
+
+
+        </body>          
+</html>
+
